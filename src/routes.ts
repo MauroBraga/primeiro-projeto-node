f132c37992f6c660529e347aad8838d7cd13a769
MauroBraga <mrb0305@gmail.com>
2020-11-02 16:03:57 -0300

    Debugando NodeJs

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 // src/routes/index.ts
 import {Router} from 'express';
+
 const routes = Router()
 
 routes.get('/', (req, res) =>{
     return res.json({message: 'Hello GoStack'});
+});
+
+routes.post('/users',(request, response) =>{
+    const { name, email } = request.body;
+
+    const user = {name, email}
+
+
+    return response.json({user});
 });
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.use(routes)
-const port = 3333;
+app.use(express.json());
+app.use(routes);
 
-app.listen(port, () => {
-    console.log(`⚡ Server started on port ${port}`);
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333');
 });
