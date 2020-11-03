c80c691e9c1ee64aefc84d47e1d8c0ff04c9b090
MauroBraga <mrb0305@gmail.com>
2020-11-03 09:26:19 -0300

    Model de Agendamento

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,36 @@
-import {  Router } from 'express';
-import { uuid } from 'uuidv4'
+import {Router } from 'express';
+import {startOfHour, parseISO,isEqual} from 'date-fns'
+
+import Appointment from '../models/Appointment';
+
+
 const appointmentsRouter = Router();
 
-const appointments = []
+
+const appointments: Appointment[] = []
+
 
 appointmentsRouter.post('/', (request, response) =>{
 
-    const {provider, data} = request.body;
+    const {provider, date} = request.body;
 
-    const appointment = {
-      id:uuid(),  provider,data
+    const parseDate = startOfHour(parseISO(date))
+
+    const findAppointmentInSameDate = appointments.find(appointment =>
+        isEqual(parseDate,appointment.date)
+    );
+
+    if(findAppointmentInSameDate){
+        return response.status(400).json({message: 'This appointment is already booked'})
     }
+
+    const appointment = new Appointment(provider,parseDate)
     appointments.push(appointment)
     return response.json(appointment);
 })
+
+appointmentsRouter.get('/', (request, response)=>{
+    return response.json(appointments)
+});
 
 export default appointmentsRouter;
