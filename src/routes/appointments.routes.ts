460f13092cefeba40089e8ae948ce992daf10df6
MauroBraga <mrb0305@gmail.com>
2020-11-08 16:08:59 -0300

    fim tratamento  de erros

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -20,15 +20,12 @@ appointmentsRouter.get('/',async (request: Request, response:Response, next:Next
 })
 
 appointmentsRouter.post('/', async (request, response) =>{
-   try{
     const {provider_id, date} = request.body;
     const parseDate = parseISO(date);
     const createAppointment = new CreateAppointmentService();
     const appointment = await createAppointment.execute({date: parseDate, provider_id})
     return response.json(appointment);
-   }catch(err){
-       return response.status(400).json({error: err.message})
-   }
+
 })
 
 
